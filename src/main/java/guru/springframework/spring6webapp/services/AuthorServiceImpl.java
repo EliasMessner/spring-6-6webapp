33ce0da6e8a4f630bacab79929d3c5e740a10c29
EliasMessner <elias.messner@unite.eu>
2023-06-21 12:59:17 +0200

    Made book and author repo final

--- src/main/java/guru/springframework/spring6webapp/services/AuthorServiceImpl.java
+++ src/main/java/guru/springframework/spring6webapp/services/AuthorServiceImpl.java
@@ -7,7 +7,7 @@ import org.springframework.stereotype.Service;
 @Service
 public class AuthorServiceImpl implements AuthorService {
 
-    private AuthorRepository authorRepository;
+    private final AuthorRepository authorRepository;
 
     public AuthorServiceImpl(AuthorRepository authorRepository) {
         this.authorRepository = authorRepository;
